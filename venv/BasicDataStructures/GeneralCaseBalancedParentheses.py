1445405ae715764f09972e45802ac9062143df93
mail4karthickr@gmail.com <mail4karthickr@gmail.com>
2019-03-23 18:27:19 -0700

    Added GeneralCase balanced parentheses checker

--- venv/BasicDataStructures/GeneralCaseBalancedParentheses.py
+++ venv/BasicDataStructures/GeneralCaseBalancedParentheses.py
@@ -1,28 +1,33 @@
 from pythonds.basic import Stack
 
 def paraChecker(symbolString):
-    stack = Stack()
-    balanced = True
     index = 0
+    balanced = True
+    stack = Stack()
+
     while index < len(symbolString) and balanced:
         symbol = symbolString[index]
-        if symbol == "(":
+
+        if symbol in "([{":
             stack.push(symbol)
         else:
             if stack.isEmpty():
                 balanced = False
             else:
-                stack.pop()
-
+                top = stack.pop()
+                balanced = matches(top, symbol)
         index = index + 1
 
-    if balanced and stack.isEmpty():
+    if stack.isEmpty() and balanced:
         return True
     else:
         return False
 
-
+def matches(open, close):
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
 
 if __name__ == "__main__":
-    result = paraChecker("((()))")
+    result = paraChecker("[{{}}]")
     print(result)
